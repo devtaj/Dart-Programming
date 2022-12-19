20212856cf9b63381e6fb91b02f9df4434e96af6
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-19 22:39:51 +0545

    Basic Calculations

---
+++ ch04.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 15, b = 60; //Declaring Variables
+
+  int sum = a + b; // adding the value of a and b
+  int diff = a - b; // minus a from b
+  int mul = a * b; // multiply a and b
+  double div = a /
+      b; // Divide a and b (It is double datatype because it's outputs numbers with decimal)
+
+  //Outputs:
+
+  print("The Sum of a and b is : $sum");
+  print("The Diff of a and b is : $diff");
+  print("The Mul of a and b is : $mul");
+  print("The Div of a and b is : $div");
+}
