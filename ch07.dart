bec7c2ec5803f4636c4440efe824753cbfb555b4
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-19 23:26:07 +0545

    Numbers Datatype

---
+++ ch07.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Datetypes
+  /*
+  -Number
+  -String 
+  -Boolean
+  -Lists
+  -Maps
+  -Sets
+  -Runes
+  -Null
+  
+   */
+
+  int a = 12;
+  double b = 35.6;
+  num c = 62;
+  num d = 65.98; // its take bool also
+
+  num sum = a + b + c + d;
+
+  //Output Value
+  print("The value of a is $a");
+  print("The value of b is $b");
+  print("The value of c is $c");
+  print("The value of d is $d");
+  print("The Sum of a,b,c and d is $sum");
+}
