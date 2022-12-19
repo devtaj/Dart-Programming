27e27eec7f1ac0f0af449888d57fffb81d4945f8
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-19 23:02:23 +0545

    Variables

---
+++ ch05.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Declaring variables
+  String name = "Dev Tajpuriya";
+  String address = "Damak";
+  int age = 19;
+  double height = 5.3;
+  bool me = true;
+
+  //Print Values
+  print("My name is $name.");
+  print("I am from $address.");
+  print("I am $age years old.");
+  print("My height is $height.");
+  print("I will always be $me.");
+}
