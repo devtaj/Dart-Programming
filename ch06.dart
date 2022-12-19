2d6ed88c09c7d8ac42e64fd4e43cf87b22126bcc
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-19 23:12:43 +0545

    Constant Variables

---
+++ ch06.dart
@@ -0,0 +1,6 @@
+void main() {
+  //Declaring constant variables
+  const pi = 3.14;
+// pi=3.24; is not working in this case, because the constant value of pi is already defined
+  print("The value of PI is $pi");
+}
