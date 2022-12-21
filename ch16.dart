f777651e0d0ea5e692a1cf2a3d7ece2358e3a1fb
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-21 18:29:32 +0545

    Assignment Operator

---
+++ ch16.dart
@@ -0,0 +1,11 @@
+void main() {
+  double age = 19;
+  age += 1;
+  print("Age after adding= $age");
+  age -= 1;
+  print("Age after subtracting= $age");
+  age *= 2;
+  print("Age after multiplying: $age");
+  age /= 2;
+  print("Age after dividing: $age");
+}
