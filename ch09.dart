ccc067e36adfdcf3dd8bf49a600a6308e64f72c3
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-21 16:59:06 +0545

    String datatype with single line and multiline

---
+++ ch09.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Single line string datatype
+
+  String FirstName = "Dev";
+  String LastName = "Tajpuriya";
+
+  print("My name is $FirstName $LastName\n\n");
+
+  //Multi-line string datatype
+  String Multiline = '''
+Dear Friend,
+            Hi, How are you?
+  ''';
+
+  print(Multiline);
+
+  // You can also use this type of quotation """ """
+  String otherMultiline = """
+Dear Friend,
+            Hi, How are you?
+  """;
+
+  print(otherMultiline);
+}
