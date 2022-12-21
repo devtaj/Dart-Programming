d1e43da68d8aea1f8de1efd50c3f50dfeff63756
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-21 21:03:08 +0545

    Properties of strin

---
+++ ch20.dart
@@ -0,0 +1,15 @@
+void main() {
+  String user = "Admin";
+  print(user.codeUnits);
+  print(user.isEmpty);
+  print(user.isNotEmpty);
+  print(user.length);
+
+  /**
+   Outputs:
+    [65, 100, 109, 105, 110]=[A,d,m,i,n]
+    false
+    true
+    5=[numbers of letter"Admin"]
+  */
+}
