665b94da8c546cb85e81373987432e4a6a74681a
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-21 18:58:46 +0545

    Relational Operators

---
+++ ch17.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 10;
+  int b = 7;
+  /**
+   relational operators
+   1. ==
+   2. >
+   3. <
+   4. >=
+   5. <=
+   
+   */
+
+  print(a == b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+}
