8955e3e3714ad3cf699d5db12e95bc4d3986e629
Dev Tajpuriya <devtaj.official@gmail.com>
2022-12-21 17:18:09 +0545

    List Datatype

---
+++ ch11.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> Names = ["Jenny", "Kelly", "Harry"];
+  //print all names in a line
+  print("All Names are :$Names");
+
+  //print all names individually
+  print("The First Name Is ${Names[0]}"); // 0 is always first value
+  print("The Second Name Is ${Names[1]}");
+  print("The Third Name Is ${Names[2]}");
+}
